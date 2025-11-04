422738c8c35d67145a6c416da4e038e7df35bec5
Alejandro Flores <alejandro.flores.alecs@gmail.com>
2025-11-04 07:43:02 -0800

    typescript intro

---
+++ typescript/calculadora/multiplicacion.ts
@@ -0,0 +1,5 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(`El resultado es: ${multiplicar(4,5)}`);
---
+++ typescript/js/javascript.js
@@ -0,0 +1,9 @@
+let mensaje = "Hola javascript";
+
+console.log(mensaje);
+console.log(typeof mensaje);
+
+mensaje = 20;
+
+console.log(mensaje);
+console.log(typeof mensaje);
